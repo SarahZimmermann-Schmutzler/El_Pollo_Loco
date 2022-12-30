cc1185ea32bdcf7c5e9cb108011b87b2caebd7a7
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2022-12-30 14:46:11 +0100

    startscreen

--- main.js
+++ main.js
@@ -7,33 +7,51 @@ function openFullscreen(element) {
     if (element.requestFullscreen) {
         element.requestFullscreen();
     } else if (element.webkitRequestFullscreen) { /* Safari */
-    element.webkitRequestFullscreen();
+        element.webkitRequestFullscreen();
     } else if (element.msRequestFullscreen) { /* IE11 */
-    element.msRequestFullscreen();
+        element.msRequestFullscreen();
     }
 
     fullscreenIconDisappear();
-  }
+}
 
-  function fullscreenIconDisappear() {
+function fullscreenIconDisappear() {
     document.getElementById('fullscreen-icon').classList.add('d-none');
     document.getElementById('nofullscreen-icon').classList.remove('d-none');
-  }
+}
 
 
-  function closeFullscreen() {
+function closeFullscreen() {
     if (document.exitFullscreen) {
-      document.exitFullscreen();
+        document.exitFullscreen();
     } else if (document.webkitExitFullscreen) { /* Safari */
-      document.webkitExitFullscreen();
+        document.webkitExitFullscreen();
     } else if (document.msExitFullscreen) { /* IE11 */
-      document.msExitFullscreen();
+        document.msExitFullscreen();
     }
 
     fullscreenIconAppear();
-  }
+}
 
-  function fullscreenIconAppear() {
+function fullscreenIconAppear() {
     document.getElementById('fullscreen-icon').classList.remove('d-none');
     document.getElementById('nofullscreen-icon').classList.add('d-none');
-  }
+}
+
+function startGame() {
+    showGame();
+    playGame();
+}
+
+function showGame() {
+    let startscreen = document.getElementById('startscreen');
+    let btnContainer = document.getElementById('btn-container');
+    let startbtn = document.getElementById('startbtn');
+    let canvas = document.getElementById('canvas');
+
+    startscreen.classList.add('d-none');
+    startbtn.classList.add('d-none');
+    btnContainer.classList.add('marginbtn');
+    canvas.classList.remove('d-none');
+}
+
